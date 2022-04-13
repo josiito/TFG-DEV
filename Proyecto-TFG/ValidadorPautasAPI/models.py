ef8e8834e6c829f144f2ead9129c301f387e3875
josiito <jose.1mayo9@gmail.com>
2022-04-13 20:40:21 +0200

    [Desarrollo] Añadido el modelo que identifica un fichero y su migración para sincronizar BD.

--- Proyecto-TFG/ValidadorPautasAPI/models.py
+++ Proyecto-TFG/ValidadorPautasAPI/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Documento(models.Model):
+    id          = models.IntegerField(primary_key=True)
+    descripcion = models.TextField(max_length=200)
+    passed      = models.BooleanField(default=False)
+    reason      = models.CharField(max_length=300, default="")
+
+    class Meta:
+        ordering = ['id']
