31342e923c0099131fc7314e4a95aabbf516f7d4
josiito <jose.1mayo9@gmail.com>
2022-04-15 23:10:54 +0200

    [Desarrollo] Añadidas las rutas necesarias. Se ha elegio implementar vistas basadas en clases.

--- Proyecto-TFG/ValidadorPautasAPI/urls.py
+++ Proyecto-TFG/ValidadorPautasAPI/urls.py
@@ -1,6 +1,20 @@
+"""
+Las URIs para los distintos recursos son:
+    - Primera pauta:  /api/comp/cont-indet
+    - Segunda pauta:  /api/comp/num-tlf
+    - Tercera pauta:  /api/comp/form-hora
+    - Cuarta pauta:   /api/comp/conec-comp
+    - Quinto recurso: /api/analisis-completo
+
+"""
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('comp/cont-indet', views.index, name='index'),
+    path('comp/cont-indet/', views.ComprobarPrimeraPauta.as_view(), name = 'DetalleDocumento'),
+    path('comp/num-tlf/', views.ComprobarSegundaPauta.as_view(), name = 'NumeroTelefono'),
+    path('comp/form-hora/', views.ComprobarTerceraPauta.as_view(), name = 'FormateoDeHora'),
+    path('comp/conec-comp/', views.ComprobarCuartaPauta.as_view(), name = 'ConcetoresComplejos'),
+    path('analisis-completo/', views.AnalisisCompleto.as_view(), name = 'AnalisisCompleto'),
 ]
