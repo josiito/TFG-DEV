c4221eb6cee52756648e97eff4605e7b162f2ca4
josiito <jose.1mayo9@gmail.com>
2022-04-17 14:55:24 +0200

    [Desarrollo] Añadidos los algoritmos necesarios para hacer las comprobaciones de las cuatro pautas.

--- Proyecto-TFG/ValidadorPautasAPI/algorithms.py
+++ Proyecto-TFG/ValidadorPautasAPI/algorithms.py
@@ -1,9 +1,11 @@
+from http.client import OK
+from pickle import NONE
+from requests import request
+from spacy.matcher import Matcher
 import spacy
 
 class Algorithms():
-    """
-    Clase que recogera los algoritmos de PLN para comprobar el cumplimiento de las cuatro pautas que se han desarrollado.
-    """
+    """ Clase que recogera los algoritmos de PLN para comprobar el cumplimiento de las cuatro pautas que se han desarrollado. """
 
     HORAS = {
         'madrugada': list([ hora for hora in range(1, 6)   ]),
@@ -12,94 +14,102 @@ class Algorithms():
         'noche'    : list([ hora for hora in range(18, 25) ]),
     }
 
-    def __init__(self, texto: str):
-        if not texto:
-            raise Exception('Es necesario pasar un texto para analizar')
+    PATRONES_PRIMERA_PAUTA = [
+        [ { "POS": "DET", "DEP": "det" } , { "POS": "NOUN", "DEP": { "IN": [ "nsubj", "obj" ] } } ],
+        [ { "MORPH": { "IS_SUPERSET": [ "PronType=Ind" ] } } ]
+    ]
+
+    PATRONES_CUARTA_PAUTA = [
+        [ { "POS": "ADP"  }, { "POS": "NOUN" }                    ], # e.j. Sin embargo
+        [ { "POS": "ADP"  }, { "POS": "PRON" }, { "POS": "NOUN" } ], # e.j. Por lo tanto
+        [ { "POS": "PART" }, { "POS": "NOUN" }                    ], # e.j. No obstante
+    ]
+
+    def __init__(self, texto: str = NONE):
+        if texto == NONE:
+            raise TypeError('No se ha encontrado ningún contenido de texto.')
 
         self.nlp = spacy.load("es_core_news_sm")
         self.doc = self.nlp(texto)
+        
+        # Intancia del matcher
+        self.matcher = Matcher(self.nlp.vocab)
 
     def validador_primera_pauta(self):
         """ Primera pauta: Se debería evitar el uso de palabras de contenido indeterminado """
-        pass
+
+        self.matcher.add('PATRONES_PRIMERA_PAUTA', self.PATRONES_PRIMERA_PAUTA)
+        matches = self.matcher(self.doc)
+
+        # Se crean la lista por la que posiblemente no cumpla la pauta
+        reason = [ self.doc[start:end] for _, start, end in matches ]
+
+        # Se elimina el patron de la cuarta pauta para evitar colisiones no deseadas
+        self.matcher.remove('PATRONES_PRIMERA_PAUTA')
+
+        # for token in self.doc:
+        #     print('Token: {} - pos: {} - dep: {} - morph: {}'.format(token.text, token.pos_, token.dep_, token.morph))
+        #     print('pos definition: {}'.format(spacy.explain(token.pos_)))
+        #     print('definicion de dependencia: {}'.format(spacy.explain(token.dep_)))
+        #     new_token = self.nlp(token.lemma_)[0]
+        #     print( 'NUEVO TOKEN MORPH ({}) -> {}'.format(new_token.text, new_token.morph) )
+            # if 'Ind' in token.morph.get('PronType'):
+            #     reason.append(token.text)
+            
+            # print('+ ------------------------------------------- +')
+        
+        return len(reason) == 0, reason
 
     def validador_segunda_pauta(self):
         """ Segunda pauta: Los numeros de telefono se deberian separar por bloques """
         
         reason: list    = []
-        passed: bool    = True
         correccion: str = ''
 
+        # Bucle para recorrer los tokens del documento analizado por spaCy
         for token in self.doc:
-
+            # Obtenemos el token que es un número
             if token.like_num:
                 num: str = token.text
-                sol_prop: str = f'{num[0:3]} {num[3:5]} {num[5:7]} {num[7:9]}'
-
+                # Comprobamos que sea un número de teléfono válido y sin separación de bloques
                 if len(num) == 9:
-                    passed = False
+                    sol_prop: str = f'{num[0:3]} {num[3:5]} {num[5:7]} {num[7:9]}'
                     reason.append('El número de telefono {} debería escribirse por bloques'.format(num))
 
-        if not passed:
-            correccion = self.doc.text.replace(num, sol_prop)
+        # Si no cumple con la pauta, se devuelve una posible corección
+        correccion = self.doc.text.replace(num, sol_prop)
         
-        return passed, reason, correccion
+        return len(reason) == 0, reason, correccion
 
     def validador_tercera_pauta(self):
         """ Tercera pauta: Se debera escribir la hora en formato 24h """
 
-        passed: bool    = True
         reason: list    = []
-        # problems: list  = []
-        # correccion: str   = ''
+
         for token in self.doc:
+            # Si el token es un token numérico o un token objeto, se analiza
             if token.pos_ in ['NUM', 'NOUN']:
                 hora = token.text.split(':')
+                # Se comprueba el formato hh:mm. Si lo cumple, no puede satisfacer la pauta
                 if len(hora) == 2:
-                    passed = False
                     reason.append('El numero {} no esta escrito en formato 24h'.format(token.text))
-                    # problems.append('{}'.format(token.text))
         
-        # if not passed:
-        #     correccion = self._corrector_tercera_pauta(problems)
-        
-        return passed, reason # , correccion
+        return len(reason) == 0, reason # , correccion
 
     def validador_cuarta_pauta(self):
         """ Cuarta pauta: Evitar el uso de conectores complejos entre oraciones """
-        pass
+
+        # Se crea el matcher con el patron correspondiente y se analiza el documento
+        self.matcher.add('PATRONES_CUARTA_PAUTA', self.PATRONES_CUARTA_PAUTA)
+        matches = self.matcher(self.doc)
+
+        reason = [ self.doc[start:end] for _, start, end in matches ]        
+        
+        # Se elimina el patron de la cuarta pauta para evitar colisiones indeseadas
+        self.matcher.remove('PATRONES_CUARTA_PAUTA')
+        
+        return len(matches) == 0, reason
 
     def analisis_completo(self):
         """ Se analizara el contenido del documento completo que se pasara como parametro """
         pass
-
-    # def _corrector_tercera_pauta(self, problems: list) -> str:
-    #     """
-    #     Corrector de la tercer pauta
-    #     """
-
-    #     texto = self.doc.text
-    #     for problem in problems:
-    #         splitter = problem.split(':')
-    #         hora, minuto = splitter
-
-    #         if minuto == '0' or minuto == '00':
-    #             dia = ''
-                
-    #             i = 0
-    #             for horas in list(self.HORAS.values()):
-    #                 print(f'Son las: {hora}')
-    #                 print(horas)
-    #                 print(f'{hora in horas}')
-    #                 print('+ ---------------------------------- +')
-    #                 if hora in horas:
-    #                     print('Son las', hora)
-    #                     dia = self.HORAS.keys()
-    #                 else:
-    #                     i += 1
-
-    #             texto = texto.replace(problem, f'{int(hora) - 12 if int(hora) > 12 else hora} de la {dia}')
-    #         else:
-    #             texto = texto.replace(problem, '-- en construccion --')
-        
-    #     return texto
