aa7766f2e54ceb4081eb358b40a412f738f21c99
josiito <jose.1mayo9@gmail.com>
2022-04-17 14:56:03 +0200

    [Corrección] El id ahora es un campo de texto para almacenar su valor hash.

--- Proyecto-TFG/ValidadorPautasAPI/models.py
+++ Proyecto-TFG/ValidadorPautasAPI/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Documento(models.Model):
-    id          = models.AutoField(primary_key=True)
+    id          = models.TextField(primary_key=True)
     descripcion = models.TextField(max_length=200)
     passed      = models.BooleanField(default=False)
     reason      = models.CharField(max_length=300, default="")
