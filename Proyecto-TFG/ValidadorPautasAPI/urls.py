e5c82d913ca808ab04d334aa81546884e319c740
josiito <jose.1mayo9@gmail.com>
2022-04-17 19:17:03 +0200

    [Corrección] Se ha cambiado el nombre mal escrito de la cuarta pauta.

--- Proyecto-TFG/ValidadorPautasAPI/urls.py
+++ Proyecto-TFG/ValidadorPautasAPI/urls.py
@@ -5,7 +5,6 @@ Las URIs para los distintos recursos son:
     - Tercera pauta:  /api/comp/form-hora
     - Cuarta pauta:   /api/comp/conec-comp
     - Quinto recurso: /api/analisis-completo
-
 """
 
 from django.urls import path
@@ -15,6 +14,6 @@ urlpatterns = [
     path('comp/cont-indet/', views.ComprobarPrimeraPauta.as_view(), name = 'Contenido Indeterminado'),
     path('comp/num-tlf/', views.ComprobarSegundaPauta.as_view(), name = 'Numero de Telefono'),
     path('comp/form-hora/', views.ComprobarTerceraPauta.as_view(), name = 'Formateo de la Hora'),
-    path('comp/conec-comp/', views.ComprobarCuartaPauta.as_view(), name = 'Concetores Complejos'),
+    path('comp/conec-comp/', views.ComprobarCuartaPauta.as_view(), name = 'Conectores Complejos'),
     path('analisis-completo/', views.AnalisisCompleto.as_view(), name = 'Analisis Completo'),
 ]
