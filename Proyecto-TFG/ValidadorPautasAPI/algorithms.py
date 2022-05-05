7969b343b9ebd8050c0ca48ae695d6f221c6272c
josiito <jose.1mayo9@gmail.com>
2022-05-05 10:40:54 +0200

    [Desarrollo] Se elimina el tercer valor devuelto del validador de la segunda pauta y se crea un metodo auxiliar.

--- Proyecto-TFG/ValidadorPautasAPI/algorithms.py
+++ Proyecto-TFG/ValidadorPautasAPI/algorithms.py
@@ -1,6 +1,5 @@
-from http.client import OK
+from operator import index
 from pickle import NONE
-from requests import request
 from spacy.matcher import Matcher
 import spacy
 
@@ -26,7 +25,8 @@ class Algorithms():
     ]
 
     def __init__(self, texto: str = NONE):
-        if texto == NONE:
+        # Si el contenido del texto es vacio o no esta instanciado, se lanza un error
+        if texto == NONE or not texto:
             raise TypeError('No se ha encontrado ningún contenido de texto.')
 
         self.nlp = spacy.load("es_core_news_sm")
@@ -46,40 +46,25 @@ class Algorithms():
 
         # Se elimina el patron de la cuarta pauta para evitar colisiones no deseadas
         self.matcher.remove('PATRONES_PRIMERA_PAUTA')
-
-        # for token in self.doc:
-        #     print('Token: {} - pos: {} - dep: {} - morph: {}'.format(token.text, token.pos_, token.dep_, token.morph))
-        #     print('pos definition: {}'.format(spacy.explain(token.pos_)))
-        #     print('definicion de dependencia: {}'.format(spacy.explain(token.dep_)))
-        #     new_token = self.nlp(token.lemma_)[0]
-        #     print( 'NUEVO TOKEN MORPH ({}) -> {}'.format(new_token.text, new_token.morph) )
-            # if 'Ind' in token.morph.get('PronType'):
-            #     reason.append(token.text)
-            
-            # print('+ ------------------------------------------- +')
         
         return len(reason) == 0, reason
 
     def validador_segunda_pauta(self):
         """ Segunda pauta: Los numeros de telefono se deberian separar por bloques """
         
-        reason: list    = []
-        correccion: str = ''
+        reason = []
+        self.__show_prints__()
 
         # Bucle para recorrer los tokens del documento analizado por spaCy
         for token in self.doc:
             # Obtenemos el token que es un número
             if token.like_num:
-                num: str = token.text
+                num = token.text
                 # Comprobamos que sea un número de teléfono válido y sin separación de bloques
                 if len(num) == 9:
-                    sol_prop: str = f'{num[0:3]} {num[3:5]} {num[5:7]} {num[7:9]}'
                     reason.append('El número de telefono {} debería escribirse por bloques'.format(num))
 
-        # Si no cumple con la pauta, se devuelve una posible corección
-        correccion = self.doc.text.replace(num, sol_prop)
-        
-        return len(reason) == 0, reason, correccion
+        return len(reason) == 0, reason
 
     def validador_tercera_pauta(self):
         """ Tercera pauta: Se debera escribir la hora en formato 24h """
@@ -113,3 +98,11 @@ class Algorithms():
     def analisis_completo(self):
         """ Se analizara el contenido del documento completo que se pasara como parametro """
         pass
+        
+    def __show_prints__(self):
+        for token in self.doc:
+            print('+ ----------------------------------------- +')
+            print('Token: {} - pos: {} - dep: {} - morph: {}'.format(token.text, token.pos_, token.dep_, token.morph))
+            print('pos definition: {}'.format(spacy.explain(token.pos_)))
+            print('definicion de dependencia: {}'.format(spacy.explain(token.dep_)))
+            print('+ ----------------------------------------- +\n')
