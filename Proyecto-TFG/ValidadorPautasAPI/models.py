a3f7b8f6ddb14ab4e5f22b626a083e8875f74d18
josiito <jose.1mayo9@gmail.com>
2022-05-17 00:51:53 +0200

    [Bug] Se ha creado un modelo que no se usa. Se debera borrar en los proximos commits.

--- Proyecto-TFG/ValidadorPautasAPI/models.py
+++ Proyecto-TFG/ValidadorPautasAPI/models.py
@@ -11,3 +11,14 @@ class Documento(models.Model):
     
     def __str__(self):
         return f'Documento {self.id} - {self.descripcion}.\n\t Cumple con las pautas?: {"Sí" if self.passed else "No"} - {self.reason if self.passed else ""}'
+
+class TextoAnalizado(models.Model):
+    id    = models.IntegerField(primary_key=True, auto_created=True)
+    texto = models.TextField(max_length=500)
+    pauta = models.IntegerField()
+
+    class Meta:
+        ordering = ['id']
+    
+    def __str__(self):
+        return f' Texto Analizado {self.id} para la pauta {self.pauta}:\n{self.texto}\n'
